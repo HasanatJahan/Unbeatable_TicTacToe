26dc5ce84d0adbe98715f7257fef98273c3814e9
Hasanat Jahan <jahanhasanat@gmail.com>
2019-07-08 23:17:09 -0400

    human input was monitored. fixing the empty arrays problem in minimax. Discovered that minimax is not working properly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
-board= [ "X", 1, 2, 
-		 3, "O", 5, 
-		 "X", 7, 8]
+board= [ 0, 1, 2, 
+		 3, 4, 5, 
+		 6, 7, 8]
 
 human = "O"
 robot= "X"
@@ -26,40 +26,40 @@ def winning(board, player):
 
 
 def evaluation(board):
-	if (winning(board, robot)):
-		return +10
-	elif (winning(board, human)):
+	if (winning(board, human)):
 		return -10
+	elif (winning(board, robot)):
+		return +10
 	else:
 		return 0
 
+################################################################################
+
 
 def minimax(spot, empty_spots, depth, maximizing_player):
+	empty_spots=list(filter(lambda spot: (spot!="O" and spot!="X"), board))
+	
 	if depth==0 or len(empty_spots)==0:
+		eval= evaluation(board)
 		# print(f'The evaluation function returns: {evaluation(board)}')
-		return evaluation(board)
+		# return evaluation(board)
+		return eval
 
 	if maximizing_player:
-		maxEval= -1000000
-		empty_spots=list(filter(lambda spot: (spot!="O" and spot!="X"), board))
-		# print(f'testing empty spots from maximizing_player{empty_spots}')
-		
+		maxEval= -1000000		
 		#for the child of that position
 		for spot in empty_spots:
 			board[spot]=robot
 			eval = minimax(spot, empty_spots, depth-1, False)
 
-			#then remove the move and replace it with the number
 			board[spot]=spot
 			maxEval= max(maxEval, eval)
-			# print(f'The maxEval is {maxEval}')
-		# print(f'The maxEval is {maxEval}')
+
 		return maxEval
 
 	else:
 		minEval= +1000000
-		# board[spot]=human
-		empty_spots=list(filter(lambda spot: (spot!="O" and spot!="X"), board))
+		# empty_spots=list(filter(lambda spot: (spot!="O" and spot!="X"), board))
 		# print(f'testing empty spots from minimizing_player{empty_spots}')
 
 		#for each child of that position
@@ -67,26 +67,36 @@ def minimax(spot, empty_spots, depth, maximizing_player):
 			board[spot]=human
 			eval= minimax(spot, empty_spots, depth-1, True)
 
-			#then remove the spot
 			board[spot]=spot
 			minEval=min(minEval, eval)
-		# print(f'The minEval is {minEval}')
+
 		return minEval
 
 
+#####################################################################################
 
 #the main program loop
 while(True):
+	#this is so that the last empty spot is accounted for 
+	#and the user is not allowed anymore inputs  
+	empty_spots=list(filter(lambda spot: (spot!="O" and spot!="X"), board))
+	if(len(empty_spots))==0:
+		eval=evaluation(board)
+		print(f'The winner is {eval}')
+		break
 
+	#this works for the human input
 	while(True):
 		try:	
 			human_input=int(input("Please enter an integer from 0 to 8: "))
 			#this should only take in empty spots
-			if(human_input>=0 and human_input<=8):
+			if(human_input>=0 and human_input<=8 and board[human_input]!="X"):
 				break
 		except ValueError:
 			print("Input must be an integer value.")	
-
+		else:
+			print("That box is not empty- try again")
+	#here you actually place the human input 
 	board[human_input]= human
 
 	#returns a list of empty positions in the array 
@@ -104,5 +114,11 @@ while(True):
 	best_move= empty_spots[0]
 	for move in moves:
 		best_move= max(best_move, move)
-	print(best_move)
+	print(f'The best move is {best_move}')
+	#this part works
+	board[best_move]=robot
 
+##############################################################################
+### THe evaluations are being accounted for?/done? wrong 
+###
+##############################################################################
